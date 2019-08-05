1204dcce7f04f9f36d0cb513ac562b6f2a0a79bd
internnexus <internfimt90@gmail.com>
2019-08-05 14:03:57 +0530

    fourth commit companyprofile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyinfoComponent } from './companyinfo/companyinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyinfoComponent
   ],
   imports: [
     BrowserModule,
